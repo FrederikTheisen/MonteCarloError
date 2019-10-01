6aa16fc018592b938c75c008ed9a73ce34eafe55
Frederik Theisen <frederiktheisen@gmail.com>
2019-10-01 10:19:52 +0200

    Added constant dist and dist selection

--- Distribution.cs
+++ Distribution.cs
@@ -6,6 +6,18 @@ namespace MonteCarloError
     {
         static readonly Random rand = new Random();
 
+        public static DistributionType Selected { get; set; } = DistributionType.Normal;
+
+        public static double Default(double mean, double stdDev)
+        {
+            return Selected switch
+            {
+                DistributionType.Constant => Constant(mean, stdDev),
+                DistributionType.Normal => Normal(mean, stdDev),
+                _ => mean,
+            };
+        }
+
         public static double Normal(double mean, double stdDev)
         {
             double u1 = 1.0 - rand.NextDouble(); //uniform(0,1] random doubles
@@ -15,5 +27,20 @@ namespace MonteCarloError
 
             return randNormal;
         }
+
+        public static double Constant(double mean, double stdDev)
+        {
+            double u = 1 - rand.NextDouble(); //uniform(0,1] random double
+            double randStd = 2 * (0.5 - u); //uniform(-1,1] random double
+            double randConstant = mean + stdDev * randStd; //random constant within mean +/- SD
+
+            return randConstant;
+        }
+    }
+
+    enum DistributionType
+    {
+        Constant,
+        Normal
     }
 }
